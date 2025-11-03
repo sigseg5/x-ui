37050a0680913c7bbe279f2d123419656246e16b
sigseg5 <sigseg5@protonmail.com>
2025-11-03 19:18:48 +0300

    hardcode link

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ install_x-ui() {
             exit 1
         fi
         echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/sigseg5/x-ui/releases/download/${last_version}/x-ui-linux-$(arch).tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch).tar.gz https://github.com/sigseg5/x-ui/archive/refs/tags/v1.12.2.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
